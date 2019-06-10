96558ddbc8eb1c6842860adb4642012d0fe83540
Rahuvich <raulmateob@gmail.com>
2019-06-10 16:47:29 +0200

    added addBookmark and editBookmark functionalities

--- bookmarks_manager/lib/src/bm_scaffold/bm_scaffold_component.dart
+++ bookmarks_manager/lib/src/bm_scaffold/bm_scaffold_component.dart
@@ -1,33 +1,69 @@
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import '../model/bookmark.dart';
 
 @Component(
   selector: 'bm-scaffold',
   templateUrl: 'bm_scaffold_component.html',
-  directives: [coreDirectives]
+  directives: [coreDirectives, formDirectives]
 )
 
 class BookmarkScaffoldComponent {
 
   final List bookmarks = [
-    {
-      'title' : "Visit my Github",
-      'description' : "Let's take a look at my work!",
-      'url':'https://github.com/Rahuvich'
-    },
-    {
-      'title' : "Visit my twitter",
-      'description' : "Follow me!",
-      'url':'https://twitter.com/raulmateob'
-    },
-    {
-      'title' : "Visit my Facebook",
-      'description' : "Take a look at my lastest posts",
-      'url':'https://twitter.com/raulmateob'
-    }
+    Bookmark(
+      title: 'Visit my Github',
+      description: "Let's take a look at my work!",
+      url: 'https://github.com/Rahuvich',
+      edit: false
+    ),
+    Bookmark(
+      title: "Visit my twitter",
+      description: "Follow me!",
+      url: 'https://twitter.com/raulmateob',
+      edit: false
+    ),
+    Bookmark(
+      title: "Visit my Facebook",
+      description: "Take a look at my lastest posts",
+      url: 'https://twitter.com/raulmateob',
+      edit: false
+    )
   ];
+
+  Bookmark editedBookmark;
+
+  addBookmark(){
+    var bm = Bookmark();
+    bookmarks.add(bm);
+    editedBookmark = Bookmark(
+      title: bm.title,
+      description: bm.description,
+      url: bm.url
+    );
+  }
 
   removeBookmark(int i){
     bookmarks.removeAt(i);
+  }
+
+  editBookmark(int i){
+    editedBookmark = Bookmark(
+      title: bookmarks[i].title, 
+      description: bookmarks[i].description,
+      url: bookmarks[i].url,
+      edit: bookmarks[i].edit
+    );
+    bookmarks[i].edit = true;
+  }
+  
+  updateBookmark(int i){
+    bookmarks[i] = Bookmark(
+      title: editedBookmark.title, 
+      description: editedBookmark.description,
+      url: editedBookmark.url,
+      edit: false
+    );
   }
 
 }
---
+++ bookmarks_manager/lib/src/model/bookmark.dart
@@ -0,0 +1,6 @@
+class Bookmark{
+  Bookmark({this.title = '', this.description = '', this.url = '', this.edit = true});
+
+  String title, description, url;
+  bool edit;
+}
